a69cdf09a6534f63cdf653bc53134001c3641f80
Abel Barbosa <78666074+abel8260@users.noreply.github.com>
2021-12-14 20:24:06 -0300

    server

--- src/iceland /index.php
+++ src/iceland /index.php
@@ -218,8 +218,7 @@
 <header class="bd-header bg-dark py-3 d-flex align-items-stretch border-bottom border-dark">
   <div class="container-fluid d-flex align-items-center">
     <h1 class="d-flex align-items-center fs-4 text-white mb-0">
-      <img src="/docs/5.1/assets/brand/bootstrap-logo-white.svg" width="38" height="30" class="me-3" alt="Bootstrap">
-      Servers. main  
+       Server:
     </h1>
     <a href="/docs/5.1/examples/cheatsheet-rtl/" class="ms-auto link-light" hreflang="ar">Iceland</a>
   </div>
